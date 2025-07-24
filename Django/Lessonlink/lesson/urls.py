092fbc4da1f8a3a5589c7b5026f873bd33d8b002
ViktorGreg <limviktorgreg14@gmail.com>
2025-07-24 16:57:52 +0800

    Updated
    Updated the URL

--- Django/Lessonlink/lesson/urls.py
+++ Django/Lessonlink/lesson/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
-    path('register_1/', views.registration_1, name='registration_1'),
-    path('register_2/', views.registration_2, name='registration_2'),
-    path('register_3/', views.registration_3, name='registration_3'),
-    path('register_4/', views.registration_4, name='registration_4'),
+    path('registration_1/', views.registration_1, name='registration_1'),
+    path('registration_2/', views.registration_2, name='registration_2'),
+    path('registration_3/', views.registration_3, name='registration_3'),
+    path('registration_4/', views.registration_4, name='registration_4'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('profile/', views.profile, name='profile'),
     path('lesson_planner/', views.lesson_planner, name='lesson_planner'),
