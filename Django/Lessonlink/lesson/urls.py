c83dd577fba214d4eb791fb373435db6c5a3bb5e
ViktorGreg <limviktorgreg14@gmail.com>
2025-07-24 15:51:29 +0800

    Error
    Asking help to fix.

--- Django/Lessonlink/lesson/urls.py
+++ Django/Lessonlink/lesson/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('register_1/', views.registration_1, name='registration_1'),
+    path('register_2/', views.registration_2, name='registration_2'),
 ]
--- Django/Lessonlink/lesson/views.py
+++ Django/Lessonlink/lesson/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def landing(request):
     return render(request, 'landing.html')
+
+def registration_1(request):
+    return render(request, 'registration_1.html')
+
+def registration_2(request):
+    return render(request, 'registration_2.html')
