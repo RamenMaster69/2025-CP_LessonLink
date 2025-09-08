ee35ec70febf62506a44710ccaac919532ebdd64
ViktorGreg <limviktorgreg14@gmail.com>
2025-09-08 11:13:22 +0800

    Debug
    Fixed urls on another project's app

--- Django/Lessonlink/lessonGenerator/urls.py
+++ Django/Lessonlink/lessonGenerator/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('generate-lesson-plan/', views.generate_lesson_plan, name='generate_lesson_plan'),
     path('lesson-plan-result/', views.lesson_plan_result, name='lesson_plan_result'),
 ]
